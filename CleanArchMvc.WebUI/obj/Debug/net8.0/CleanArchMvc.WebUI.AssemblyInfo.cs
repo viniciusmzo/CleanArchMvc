abbe9673ef5f346502762a65b4295478fd1ab49e
Vinicius Marques <vinicius_mzo@hotmail.com>
2024-05-14 15:34:33 -0300

    Adding endpoint to details category

--- CleanArchMvc.WebUI/obj/Debug/net8.0/CleanArchMvc.WebUI.AssemblyInfo.cs
+++ CleanArchMvc.WebUI/obj/Debug/net8.0/CleanArchMvc.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchMvc.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8d4d10f8abb24f72b8a1fbac2deb7c3a3a98cde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9f48cb5f63f583e7a970e6c51a18bcc8e86b9b4")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchMvc.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchMvc.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
