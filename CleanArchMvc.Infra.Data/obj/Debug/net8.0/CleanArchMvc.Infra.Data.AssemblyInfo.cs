d16e051ac54922f5fc5901c273403aca549043c0
Vinicius Marques <vinicius_mzo@hotmail.com>
2024-05-15 19:36:19 -0300

    Implementing the IAuthenticate interface

--- CleanArchMvc.Infra.Data/obj/Debug/net8.0/CleanArchMvc.Infra.Data.AssemblyInfo.cs
+++ CleanArchMvc.Infra.Data/obj/Debug/net8.0/CleanArchMvc.Infra.Data.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CleanArchMvc.Infra.Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcae53d2428ccc179cbb13e4ed47c3f938373fe7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee8c3e08990e88e92365fc4e09b0e0166938f093")]
 [assembly: System.Reflection.AssemblyProductAttribute("CleanArchMvc.Infra.Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CleanArchMvc.Infra.Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
